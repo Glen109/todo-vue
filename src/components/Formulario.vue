9e7c8dfa1c6274573b815cbe260a941d0dc9144d
Glen109 <gledson10rodial@gmail.com>
2025-11-11 15:00:52 -0300

    componetizando

---
+++ src/components/Formulario.vue
@@ -0,0 +1,23 @@
+<script setup>
+    const pops = defineProps(['cadastraTarefa', 'tarefaTemp', 'trocarFiltro', 'editaTarefaTemp']);
+</script>
+<template>
+    <form @submit.prevent="pops.cadastraTarefa">
+        <div class="row">
+            <div class="col">
+                <input :value="pops.tarefaTemp" @change="pops.editaTarefaTemp" required
+                    type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
+            </div>
+            <div class="col-md-2">
+                <button class="btn btn-primary">Cadastrar</button>
+            </div>
+            <div class="col-md-2">
+                <select @change="pops.trocarFiltro" class="form-control">
+                    <option value="todas">Todas as Tarefas</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="finalizadas">Finalizadas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,21 @@
+<script setup>
+const pops = defineProps(['tarefas', 'numeroTarefasPendentes', 'estados']);
+</script>
+<template>
+    <ul v-if="(pops.estados.filtro !== 'finalizadas' && pops.estados.filtro !== 'todas' && numeroTarefasPendentes.length === 0) == false" class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in pops.tarefas">
+            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+                :id="tarefa.titulo" type="checkbox">
+            <label :class="{ done: tarefa.finalizada === true }" class="ms-3" :for="tarefa.titulo">
+                {{ tarefa.titulo }}
+            </label>
+        </li>
+    </ul>
+    <h2 class="mt-3 text-center" v-else-if="pops.estados.filtro == 'pendentes' && numeroTarefasPendentes.length === 0" >Não há Tarefas</h2>
+</template>
+
+<style scoped>
+    .done {
+        text-decoration: line-through;
+    }
+</style>
